d87d872216ce15e9edfc614c21fc7b25b97c5f2d
james <james@DESKTOP-JDJA6NK>
2020-11-19 22:57:13 +0000

    - Added references to CodeBuilder

--- Expressions/TSMethodInvoke.cs
+++ Expressions/TSMethodInvoke.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using CodeBuilder.Expressions;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace TSCodeBuilder.Expressions
 {
-    class TSMethodInvoke
+    class TSMethodInvoke : MethodInvoke
     {
+        public TSMethodInvoke(string name, params string[] args)
+        {
+            this.MethodName = name;
+        }
     }
 }
